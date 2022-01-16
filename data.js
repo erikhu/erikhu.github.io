57dd0a6081e2cec5d835d6661ee7f266f03f544d
Erik Gonzalez <erikhuboy@gmail.com>
2022-01-15 23:55:28 -0500

    update

---
+++ data.js
@@ -0,0 +1,104 @@
+var dataJson = {
+    "author": "Erik Gonzalez",
+    "currentRole": "Full-Stack",
+    "last_updated": "2022-01-15",
+    "side_sections": [
+        {
+            "title": "Skills",
+            "list": [
+                "Problem solving",
+                "Algorithms",
+                "Software develop",
+                "Good communication",
+                "Gnu/Linux native",
+                "Intermediate english level",
+                "Spanish native"
+            ],
+        },
+        {
+            "title": "Frameworks",
+            "list": [
+                "Laravel",
+                "Ruby on rails",
+                "Phoenixframework",
+                "Vuejs",
+                "React Native",
+                "Android"
+            ],
+        },
+        {
+            "title": "Languages",
+            "list": [
+                "Ruby",
+                "Php",
+                "Elixir",
+                "Javascript",
+                "Typescript",
+                "Java"
+            ],
+        },
+        {
+            "title": "Tools",
+            "list": [
+                "Emacs (Evil)",
+                "Docker",
+                "Terminal/Console"
+            ],
+        },
+        {
+            "title": "Hoobies",
+            "list": [
+                "Hikking",
+                "Read",
+                "Animal lover",
+                "Anime",
+                "Sport"
+            ]
+        },
+        {
+            "title": "Favorite books",
+            "list": [
+                "Object Design",
+                "Algorithms by robert sedgewick",
+                "Clean code"
+            ]
+        }
+    ],
+    "bio": "Welcome, I will tell you a bit about me. I am a guy that loves learning new things about Tech and Software.  I always try to see the news about the open-source community and big companies in general related to tech. I learn as an autodidact, self-study is my way to learn everything, I prefer to read technical books. Always I am reading some book related to software architecture, language, or just new proposals related to software. My native language is Spanish, but I have conversational English. I am open for learn some new human or computer languages too. Another thing that I enjoy is being close with many kinds of people, having good vibes, and sharing ideas, knowledge, or just a normal chat. If you would like, we can take a little talk about software at any time just tell me with some time, I enjoy that kind of conversation.",
+    "experiences": [
+        {
+            "date": "2014",
+            "company": "Neurotec",
+            "role": "Techincal Support",
+            "description": "I gave support to the System fields of a Colombian company.",
+            "activities": []
+        },
+        {
+            "date": "2015",
+            "company": "Coviper",
+            "role": "Software developer",
+            "description": "A company that I was founded with a couple of friends and I did web pages with WordPress, created plugins and mobile apps in Java.",
+            "activities": []
+        },
+        {
+            "date": "2017",
+            "company": "Freelance",
+            "role": "Software developer",
+            "description": "I developed several web projects using docker and Mobile apps like",
+            "activities": [
+                "Apartado Stereo (App in ionic) with a basic Laravel web to admin some stuff that the app used by API.",
+                "Callvision (frontend) was made in ionic with angular 4.",
+                "I-Lead web app to create e-cards, I made in RoR and dockerized.",
+                "Alentreno was another platform made in RoR and dockerized.",
+                "RadiosFlow more than 30 apps of many countries created to hear broadcasting radio stations in my free time. I have made a command line to automatize the deployment and compilation of all the apps with the same app in ruby."
+            ]
+        },
+        {
+            "date": "2020",
+            "company": "PicallEx",
+            "role": "Full-Stack",
+            "description": "I create several projects in the company working. I have to test and use Php, Elixir, Js, Html, Css, Ts in my daily to add new functionalities, maintain projects and create new ones. I worked with Real-time applications using phoenix elixir and Vuejs connecting through WebSockets.  I have to maintain projects with several frameworks like React native, Vuejs, Phoenixframework, and Laravel. Also, I contribute to the main project of the company and  API of the working under the rules based on the domain required.",
+            "activities": []
+        }
+    ]
+}
